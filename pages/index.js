a14a95c2a8a033f3cdf12a1481bf200972be5bcb
John Alex <85435945+JohnDalex10@users.noreply.github.com>
2022-03-30 17:36:59 +0100

    just js!

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">React Life!</a>
+          Welcome to <a href="https://nextjs.org">JavaScript Life!</a>
         </h1>
 
         <p className={styles.description}>
